e25d5eab977084f4f908d14bcf1ff1f80b7207f8
yankai <yanakai@126.com>
2019-09-15 10:46:34 +0800

    增加RestTemplate+Ribbon类型消费和Feign消费类型注释 Feign 自带Ribbon

--- springcloud/eureka-feign/src/main/java/com/yk/eurekafeign/EurekaFeignApplication.java
+++ springcloud/eureka-feign/src/main/java/com/yk/eurekafeign/EurekaFeignApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
+//通过Feign去消费服务 Feign默认集成了Ribbon，并和Eureka结合，默认实现了负载均衡的效果
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
--- springcloud/eureka-ribbon/src/main/java/com/yk/eurekaribbon/EurekaRibbonApplication.java
+++ springcloud/eureka-ribbon/src/main/java/com/yk/eurekaribbon/EurekaRibbonApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+//RestTemplate+Ribbon去消费服务
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient //向服务中心注册
