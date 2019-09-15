7f3735951cf787395e01e3b0a6858f5f43e24e03
yankai <yanakai@126.com>
2019-09-15 12:10:37 +0800

    Ribbon增加Hystrix 断路器配置

--- springcloud/eureka-ribbon/src/main/java/com/yk/eurekaribbon/EurekaRibbonApplication.java
+++ springcloud/eureka-ribbon/src/main/java/com/yk/eurekaribbon/EurekaRibbonApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient //向服务中心注册
+@EnableHystrix   //注解开启Hystrix断路器
 public class EurekaRibbonApplication {
 
     public static void main(String[] args) {
--- springcloud/eureka-ribbon/src/main/java/com/yk/eurekaribbon/HelloCloudService.java
+++ springcloud/eureka-ribbon/src/main/java/com/yk/eurekaribbon/HelloCloudService.java
@@ -1,29 +1,22 @@
 package com.yk.eurekaribbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
-/**
- * @ProjectName: springcloud
- * @Package: com.yk.eurekaribbon
- * @ClassName: HelloCloudService
- * @Description: java类作用描述
- * @Author: yanakai@126.com
- * @CreateDate: 2019/9/14 15:47
- * @UpdateUser: yanakai@126.com
- * @UpdateDate: 2019/9/14 15:47
- * @UpdateRemark: 更新说明
- * @Version: 1.0
- */
 @Service
 public class HelloCloudService {
     @Resource
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name){
-        return restTemplate.getForObject("http://EUREKA-CLIENT/hi?name="+
-                .3,String.class);
+        return restTemplate.getForObject("http://EUREKA-CLIENT/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
